421c7736ef84a68ba906afa90bd62da1977e41f0
Superman <supermanchc@gmail.com>
2018-05-31 23:11:50 +0800

    docs: add highstock and highmaps demo

---
+++ demo/components/Map.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="charts">
+        <h3>Map</h3>
+        <vue-highcharts :highcharts="Highcharts" :options="options" ref="drilldownChart"></vue-highcharts>
+    </section>
+</template>
+<script>
+import VueHighcharts from '../../src/VueHighcharts.vue'
+import Highcharts from 'highcharts/highmaps'
+import Drilldown from '../../node_modules/highcharts/modules/drilldown.js'
+import { data } from '../../data/mapData.js'
+
+export default {
+  components: {
+    VueHighcharts,
+  },
+  data() {
+    return {
+      Highcharts: Highcharts,
+      options: {
+        title: {
+          text: 'Habitat of the Rusty Blackbird',
+        },
+
+        subtitle: {
+          text:
+            'An example of a distribution map in Highcharts.<br/>' +
+            'Source: <a href="http://en.wikipedia.org/wiki/File:Euphagus_carolinus_map.svg">Wikipedia</a>.',
+        },
+
+        plotOptions: {
+          series: {
+            tooltip: {
+              headerFormat: '',
+              pointFormat: '{series.name} area',
+            },
+          },
+        },
+
+        legend: {
+          align: 'left',
+          backgroundColor:
+            (Highcharts.theme && Highcharts.theme.legendBackgroundColor) ||
+            'rgba(255, 255, 255, 0.85)',
+          floating: true,
+          layout: 'vertical',
+          verticalAlign: 'bottom',
+          reversed: true,
+        },
+        series: data,
+      },
+    }
+  },
+}
+</script>
---
+++ demo/components/Stock.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="charts">
+        <h3>Stock Chart</h3>
+        <vue-highcharts :highcharts="Highcharts" :options="options" ref="drilldownChart"></vue-highcharts>
+    </section>
+</template>
+<script>
+import VueHighcharts from '../../src/VueHighcharts.vue'
+import Highcharts from 'highcharts/highstock'
+import Drilldown from '../../node_modules/highcharts/modules/drilldown.js'
+import { data } from '../../data/stockData.js'
+
+const stockData = data.map(d => d.slice(0.2))
+console.log(stockData)
+export default {
+  components: {
+    VueHighcharts,
+  },
+  data() {
+    return {
+      Highcharts: Highcharts,
+      options: {
+        rangeSelector: {
+          selected: 2,
+        },
+        title: {
+          text: '平安银行历史股价',
+        },
+        plotOptions: {
+          series: {
+            showInLegend: true,
+          },
+        },
+        tooltip: {
+          split: false,
+          shared: true,
+        },
+        series: [
+          {
+            // type: 'line',
+            id: '000001',
+            name: '平安银行',
+            data: stockData,
+          },
+        ],
+      },
+    }
+  },
+}
+</script>
